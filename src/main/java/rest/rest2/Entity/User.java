cb6f368b83bcac2a3e62aca50f2ed1a93a2a5ac4
GladiatorMPVZ <73162149+GladiatorMPVZ@users.noreply.github.com>
2022-12-17 16:16:41 +0300

    add role and users

---
+++ src/main/java/rest/rest2/Entity/User.java
@@ -0,0 +1,28 @@
+package rest.rest2.Entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "pass")
+    private String pass;
+
+    @ManyToMany
+    @JoinTable(
+
+    )
+}
